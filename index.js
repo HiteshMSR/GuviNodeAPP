4e9bad0799327e9929492bee6afa1b057183d0d0
HiteshMSR <msrhitesh@gmail.com>
2022-07-18 13:04:03 +0530

    Changed to local url

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express()
 dotenv.config()
 app.use(cors({origin : "*"}))
 // const MONGO_URL = "mongodb://127.0.0.1";
-// const MONGO_URL="mongodb+srv://hitesh:hitesh@cluster0.cquwee5.mongodb.net/";
-const MONGO_URL=process.env.MONGO_URL;
+const MONGO_URL="mongodb+srv://hitesh:hitesh@cluster0.cquwee5.mongodb.net/";
+// const MONGO_URL=process.env.MONGO_URL;
 
 const movies1=[
   {
